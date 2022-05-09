2988a5520bb746e27b3463bcc2f11ccaac2d17e5
Alisson Youssf <youssfbr@gmail.com>
2022-05-09 19:45:17 -0300

    backend, frontend project created

---
+++ backend/src/main/java/com/github/youssfbr/movie/Movie2022Application.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Movie2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Movie2022Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/youssfbr/movie/Movie2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.movie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Movie2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
