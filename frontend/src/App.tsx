8978c64c9ad3a56c6e39268c6a80e2a451a70bec
Alisson Youssf <youssfbr@gmail.com>
2022-05-10 23:08:57 -0300

    Frontend Routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,28 @@
-import Footer from "shared/components/Footer";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Listing from 'pages/Listing';
+import Form from 'pages/Form';
+
 import Navbar from "./shared/components/Navbar";
+import Footer from "shared/components/Footer";
 
 function App() {
   return (
-    <div>
-      <Navbar /> 
-      <Footer />   
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Listing />} />
+        <Route path="/form">
+          <Route path=":movieId" element={<Form />} />
+        </Route>
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
 export default App;
+
